683223a8a905ca02a6a2208753971a121458cdaf
Salvatore Napolitano <salvo.napo1972@gmail.com>
2026-07-12 16:49:17 +0200

    Canti regina apostolorum

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,16 +2,49 @@ import {
   streamText,
   UIMessage,
   convertToModelMessages,
+  tool,
+  isStepCount,
   createUIMessageStreamResponse,
   toUIMessageStream,
 } from 'ai';
+import { z } from 'zod';
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
-    model: "xai/grok-4",
+    model: "xai/grok-4.3",
     messages: await convertToModelMessages(messages),
+    stopWhen: isStepCount(5),
+    tools: {
+      weather: tool({
+        description: 'Get the weather in a location (fahrenheit)',
+        inputSchema: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+      convertFahrenheitToCelsius: tool({
+        description: 'Convert a temperature in fahrenheit to celsius',
+        inputSchema: z.object({
+          temperature: z
+            .number()
+            .describe('The temperature in fahrenheit to convert'),
+        }),
+        execute: async ({ temperature }) => {
+          const celsius = Math.round((temperature - 32) * (5 / 9));
+          return {
+            celsius,
+          };
+        },
+      }),
+    },
   });
 
   return createUIMessageStreamResponse({
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -15,6 +15,13 @@ export default function Chat() {
             switch (part.type) {
               case 'text':
                 return <div key={`${message.id}-${i}`}>{part.text}</div>;
+              case 'tool-weather':
+              case 'tool-convertFahrenheitToCelsius':
+                return (
+                  <pre key={`${message.id}-${i}`}>
+                   
+                  </pre>
+                );
             }
           })}
         </div>
