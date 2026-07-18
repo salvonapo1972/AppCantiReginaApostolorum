3e3cc2bce51c026c9cfdf22587ff6ad79fb5d21a
Salvatore Napolitano <salvo.napo1972@gmail.com>
2026-07-18 18:57:55 +0200

    Canti regina apostolorum

--- app/songs/[slug]/page.tsx
+++ app/songs/[slug]/page.tsx
@@ -58,7 +58,7 @@ export default async function SongPage({ params }: SongPageProps) {
        <div className="mb-10 text-right">
                 <Link className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href={`/elenco`}>Torna Indietro</Link>
               </div>
-      <div className="">
+      <div className="canto">
         <div className="[&>p]:mb-8 [&>h1]:font-extrabold [&>h1]:text-2xl text-center">
           {documentToReactComponents(testoCanzone, options)}
         </div>
