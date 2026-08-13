b2a8a7b91d8de2aa6cadaf7743e0bd7851e47f8d
salvonapo1972 <salvo.napo1972@gmail.com>
2026-08-13 22:41:34 +0200

    Update coordinates header in sendMessage call

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -169,7 +169,7 @@ export default function Chat() {
       { text: input }, 
       { 
         headers: {
-          coordinates: coords 
+          coordinates: 'testttt',
         }
       }
     );
