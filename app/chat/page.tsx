a421bf882ee48b635d51fbd34505745a06cff300
salvonapo1972 <salvo.napo1972@gmail.com>
2026-08-13 21:47:02 +0200

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -43,7 +43,7 @@ export default function Chat() {
             id,
             messages,
             trigger,
-            ...(requestMetadata as any || {}),
+            metadata:{coordinates:coords},
           },
         };
       },
