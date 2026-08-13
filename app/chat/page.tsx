8767a0225380ae7b53bddb3dc9dd119fbc31b092
salvonapo1972 <salvo.napo1972@gmail.com>
2026-08-13 20:51:15 +0200

    Fix formatting in sendMessage call

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -169,7 +169,7 @@ export default function Chat() {
               { text: input,
                   
                 coordinates: coords, 
-                  
+              }
                
             );
             
