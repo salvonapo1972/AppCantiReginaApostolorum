9d9b8a91982d7d115d328a85061318a1d3952048
salvonapo1972 <salvo.napo1972@gmail.com>
2026-08-26 13:27:30 +0200

    Update gplstations.ts

--- app/lib/gplstations.ts
+++ app/lib/gplstations.ts
@@ -28,7 +28,7 @@ export async function getGplStations() {
 
     // 3. Seleziona i blocchi delle stazioni (adatta il selettore se la struttura classi del sito cambia)
     // Basato sulla struttura standard del testo estratto:
-    $('.container, [class*="station-"]').each((index, element) => {
+    $('.wrap #container, [class*="station"]').each((index, element) => {
       const container = $(element).closest('div'); // Trova il contenitore principale della stazione
       
       // Estrazione del nome e dell'indirizzo
